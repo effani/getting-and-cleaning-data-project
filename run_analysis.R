b63a92955d7e8c4ac8b4fef5c16d47598014f16f
effani <no271828lizabeth@yahoo.com>
2025-10-08 15:26:01 -0600

    Complete tidy dataset, start summary

--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,43 @@
 library(dplyr)
 
-## Make dictionary to change labels from numbers to activity types
+## import data
+features <- readLines("data/features.txt")
 activity_labels <- readLines("data/activity_labels.txt")
 
-# Use substr() to extract label (first character of each line)
-# and activity (characters 3 to end of line)
-# use 50 as arbitrarily high value for end to ensure the whole line is captured
-activity_label_df <- data.frame(label = substr(activity_labels, 1, 1),
-                                activity = substr(activity_labels, 3, 50))
+train_data <- readLines("data/train/X_train.txt")
+train_labels <- readLines("data/train/y_train.txt")
+train_subjects <- readLines("data/train/subject_train.txt")
+
+test_data <- readLines("data/test/X_test.txt")
+test_labels <- readLines("data/test/Y_test.txt")
+test_subjects <- readLines("data/test/subject_test.txt")
 
 
-## idk i'm figuring it out
-features <- readLines("data/features.txt")
-x_train <- readLines("data/train/X_train.txt")
+## Change activity labels into a named vector that can be used as a dictionary
 
-# Each line in the "X" files (X_train.txt and X_test.txt) contains 561 elements
+names(activity_labels) <- substr(activity_labels, 1, 1)
+activity_labels <- substr(activity_labels, 3, 50)
+# Set end value for substr() at 50
+# this is longer than any of the strings 
+# to ensure the whole activity name is captured
+
+
+## Remove initial numbers from features list
+## and determine which features contain mean and std
+split_features <- strsplit(features," ")
+features <- sapply(split_features, function(x) x[[2]])
+mean_or_sd <- grepl("mean\\(\\)|std\\(\\)", features)
+rm(split_features)
+
+
+## Clean and merge data files
+
+# Each line in the data files (X_train.txt and X_test.txt) contains 561 elements
 # corresponding to the list of features. All elements are numbers in scientific
 # notation, stored in text format.
-# The clean_data() function is intended to split these lines into a numeric
-# vector.
-clean_x_data <- function(xline) {
+# This function splits each of these lines into a numeric vector.
+
+clean_data <- function(xline) {
   xlist <- strsplit(xline, " ")
   # strsplit() returns a list whose only element is a character vector.
   # extract the vector, i.e. the first element of the list
@@ -32,14 +50,42 @@ clean_x_data <- function(xline) {
   xnumeric[!is.na(xnumeric)]
 }
 
-x_train_list <- lapply(x_train, clean_x_data)
-# this is a list of 7352 observations, each of which is a numeric vector with
+train_list <- lapply(train_data, clean_data)
+test_list <- lapply(test_data, clean_data)
+all_data_list <- c(train_list, test_list)
+# this is a list of 10299 observations, each of which is a numeric vector with
 # 561 elements 
+
+# save memory by removing partial datasets
+rm(train_data, test_data, train_list, test_list)
 
 # make each element of the list into one row of a dataframe
 # and set the column names to the features vector (from features.txt)
-x_train_df <- do.call("rbind", x_train_list)
-colnames(x_train_df) <- features
+all_data <- do.call("rbind", all_data_list)
+# This produces a matrix. Add column names and then use dplyr::as_tibble to 
+# make a dataframe that will print relatively neatly.
 
-# remove unnecessary intermediates to save memory
-rm(x_train, x_train_list)
+colnames(all_data) <- features
+all_data <- as_tibble(all_data)
+
+
+## Extract only mean and standard deviation for each measurement
+all_data <- all_data[, mean_or_sd]
+
+
+## Add activity names and subjects to dataframe
+subject <- as.integer(c(train_subjects, test_subjects))
+# convert subject to integer to improve sorting
+
+labels <- c(train_labels, test_labels)
+
+all_data <- cbind(all_data, labels, subject) %>%
+  mutate(activity = activity_labels[labels])
+
+
+## Create summary data table
+data_summary <- all_data %>%
+  group_by(subject, activity) %>%
+  summarise(avgtBodyAcc_X = mean(`tBodyAcc-mean()-X`))
+
+
